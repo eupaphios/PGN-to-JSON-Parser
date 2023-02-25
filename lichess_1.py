52f34c86a288e07d0f57e83c1386afaaf1b8a008
ognn <ogunozalp1991@gmail.com>
2023-02-25 23:51:31 +0300

    sadas

--- lichess_1.py
+++ lichess_1.py
@@ -10,7 +10,6 @@ headers = {"Accept": "application/x-ldjson"}
 response = requests.get(url, headers=headers, stream=True)
 list_resp = response.text.splitlines()
 json_resp = list(map(lambda x: json.loads(x), list_resp))
-i = 2
 lmlist = []
 for i in range(len(json_resp)):
     if (not json_resp[i].get('lm') is None):
